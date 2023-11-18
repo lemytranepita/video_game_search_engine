cf3324c1ba04d87da8ee781458c6ce5000745930
lemytranepita <le-my.tran@epita.fr>
2023-11-18 17:15:11 +0100

    Json file into queue in RabbitMQ

--- file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
@@ -1,16 +1,35 @@
 package fr.lernejo.fileinjector;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class Launcher {
 
-    public static void main(String[] args) {
-        try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
+    public static void main(String[] args) throws IOException {
+        try (AnnotationConfigApplicationContext springApplicationContext = new AnnotationConfigApplicationContext(Launcher.class)) {
 
-            System.out.println("Hello after starting Spring");
+            RabbitTemplate rabbitTemplate = springApplicationContext.getBean(RabbitTemplate.class);
+            ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+            CollectionType listType = objectMapper.getTypeFactory().constructCollectionType(List.class, Map.class);
+
+            List<Map<String, Object>> messages = objectMapper.readValue(new File(args[0]), listType);
+
+            messages.forEach(message -> {
+                MessageProperties messageProperties = new MessageProperties();
+                messageProperties.setContentType(MessageProperties.CONTENT_TYPE_JSON);
+                messageProperties.setHeader("id", message.get("id"));
+                rabbitTemplate.send("game_info", rabbitTemplate.getMessageConverter().toMessage(message, messageProperties));
+            });
         }
     }
 }
